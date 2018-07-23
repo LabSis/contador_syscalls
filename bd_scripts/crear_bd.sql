7db1f15e5fd887963cf636851ec52fa1be5690fd
fdrcbrtl <federicojbertola@gmail.com>
2018-07-23 00:03:04 -0300

    Script de creación de BD inicial

---
+++ bd_scripts/crear_bd.sql
@@ -0,0 +1,66 @@
+DROP DATABASE IF EXISTS gestion_ransomware;
+CREATE DATABASE IF NOT EXISTS gestion_ransomware;
+USE gestion_ransomware;
+
+CREATE TABLE ransomwares(
+	id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    descripcion VARCHAR(500),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE parametros(
+	id INT NOT NULL AUTO_INCREMENT,
+    clave VARCHAR(50),
+    valor VARCHAR(150),
+    id_ransomware INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_ransomware) REFERENCES ransomwares (id)
+);
+
+CREATE TABLE pruebas(
+	id INT NOT NULL AUTO_INCREMENT,
+    id_ransomware INT NOT NULL,
+    cantidad_datos INT,
+    cantidad_archivos INT,
+    detector_habilitado BIT,
+    deteccion_positiva BIT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_ransomware) REFERENCES ransomwares (id)
+);
+
+CREATE TABLE syscall_resultados(
+	id INT NOT NULL AUTO_INCREMENT,
+    syscall VARCHAR(50),
+    cantidad INT,
+    id_prueba INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_prueba) REFERENCES pruebas (id)
+);
+
+CREATE TABLE procesamiento_resultados(
+	id INT NOT NULL AUTO_INCREMENT,
+    user DOUBLE,
+    system DOUBLE,
+    id_prueba INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_prueba) REFERENCES pruebas (id)
+);
+
+CREATE TABLE disco_resultados(
+	id INT NOT NULL AUTO_INCREMENT,
+    iowait DOUBLE,
+    idle DOUBLE,
+    id_prueba INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_prueba) REFERENCES pruebas (id)
+);
+
+CREATE TABLE memoria_resultados(
+	id INT NOT NULL AUTO_INCREMENT,
+    memused DOUBLE,
+    porcentaje_memused DOUBLE,
+    id_prueba INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_prueba) REFERENCES pruebas (id)
+);
